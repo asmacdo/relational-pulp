68d4142889aae50d62ea01bca715e38d14467125
Austin Macdonald <asmacdo@gmail.com>
2016-08-19 16:49:36 -0400

    I kinda like this

--- pulp/views.py
+++ pulp/views.py
@@ -4,23 +4,28 @@ import json
 from rest_framework import filters, routers, viewsets
 
 
-class JSONQueryParamViewSet(viewsets.ModelViewSet):
+class JSONFilterViewSet(viewsets.ModelViewSet):
+    """
+    Allows ViewSet writer to explicitly allow lookup expressions that require JSON parsing.
+    """
+    whitelist_json_filters = []
 
     def get_queryset(self):
         qs = self.queryset
         for key, value in self.request.query_params.items():
-            if key.endswith('__in'):
+            if key in self.whitelist_json_filters:
                 qs_filter = {key: json.loads(value)}
                 qs = qs.filter(**qs_filter)
-
         return qs
 
 
-class RepositoryViewSet(JSONQueryParamViewSet):
+class RepositoryViewSet(JSONFilterViewSet):
+    whitelist_json_filters = ['slug__in']
     lookup_field = 'slug'
     queryset = models.Repository.objects.all()
     serializer_class = serializers.RepositorySerializer
     filter_backends = (filters.DjangoFilterBackend,)
+
 
 # XXX DO NOT register ContentUnitViewSet with the router.
 # It's here to be subclasses by the specific unit types,
