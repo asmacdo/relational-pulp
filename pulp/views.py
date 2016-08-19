53901774fa349ef5ce6fb5f787c6dbc5d8795e3a
Austin Macdonald <asmacdo@gmail.com>
2016-08-19 15:21:53 -0400

    Demonstate how to create an "In" search
    Probably bad
    
    I kinda like this
    
    this is the way

--- pulp/views.py
+++ pulp/views.py
@@ -1,12 +1,37 @@
 from pulp import models, serializers
 
+import django_filters
+from rest_framework import filters
 from rest_framework import routers, viewsets
+
+
+class CharInFilter(django_filters.filters.BaseInFilter,
+                   django_filters.filters.CharFilter):
+    pass
+
+
+class RepositoryFilter(filters.FilterSet):
+    """
+    Allows this search:
+    http 'http://example/api/v3/repositories/?slug_in_list=singing-gerbil,versatile-pudu'
+    """
+
+    # Potential 'gotcha' here: This field cannot be called "slug__in" because django_filters
+    # recognizes this as special syntax and tries to do its own stuff to it. (I think it looks for
+    # and `in` sub-field within the slug field.
+    slug_in_list = CharInFilter(name='slug', lookup_expr='in')
+
+    class Meta:
+        model = models.Repository
+        fields = ['slug_in_list']
 
 
 class RepositoryViewSet(viewsets.ModelViewSet):
     lookup_field = 'slug'
     queryset = models.Repository.objects.all()
     serializer_class = serializers.RepositorySerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = RepositoryFilter
 
 
 # XXX DO NOT register ContentUnitViewSet with the router.
--- pulp_rpm/views.py
+++ pulp_rpm/views.py
@@ -1,11 +1,27 @@
-from pulp.views import ContentUnitViewSet, router
+from pulp.views import ContentUnitViewSet, CharInFilter, router
+from pulp import models as pulp_models
 from pulp_rpm import models, serializers
+
+from rest_framework import filters
+
+
+# class RPMFilter(filters.FilterSet):
+#     """
+#     """
+#     # in_repo = CharInFilter(name='repositories', lookup_expr='in')
+
+#     class Meta:
+#         model = models.RPM
+#         fields = ['repositories']
 
 
 # TODO: Magic trick: autogenerate viewsets by finding contentunit model classes
 class RPMViewSet(ContentUnitViewSet):
     queryset = models.RPM.objects.all()
     serializer_class = serializers.RPMSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_fields = ('name',)
+    # filter_class = RPMFilter
 
 
 class SRPMViewSet(ContentUnitViewSet):
