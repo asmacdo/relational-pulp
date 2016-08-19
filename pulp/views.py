c409bbd89311561859467c50f920df834b0c54b1
Austin Macdonald <asmacdo@gmail.com>
2016-08-19 15:21:53 -0400

    Hey, this works!

--- pulp/views.py
+++ pulp/views.py
@@ -1,12 +1,28 @@
 from pulp import models, serializers
 
-from rest_framework import routers, viewsets
+import json
+import django_filters
+from rest_framework import filters, routers, viewsets
+
+
+class RepositoryFilter(filters.FilterSet):
+    repo__in = django_filters.MethodFilter(action='in_list_filter')
+
+    class Meta:
+        model = models.Repository
+        fields = ['slug', 'repo__in']
+
+    def in_list_filter(self, queryset, value):
+        value = json.loads(value)
+        return queryset.filter(slug__in=value)
 
 
 class RepositoryViewSet(viewsets.ModelViewSet):
     lookup_field = 'slug'
     queryset = models.Repository.objects.all()
     serializer_class = serializers.RepositorySerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = RepositoryFilter
 
 
 # XXX DO NOT register ContentUnitViewSet with the router.
